148f76d63be3d0477ca2ab8bd2d1fb622ac75d48
yhsyn0 <yhsyn1@gmail.com>
2021-05-30 22:01:29 +0300

    Up Triggers, Procs

--- new-query.sql
+++ new-query.sql
@@ -12,9 +12,10 @@ CREATE TABLE Polyclinic
 
 CREATE TABLE Doctor
 (
-	Id		int PRIMARY KEY NOT NULL,
-	FirstName		varchar(255) NOT NULL,
-	LastName		varchar(255) NOT NULL,
+	Id			int PRIMARY KEY NOT NULL,
+	FirstName	varchar(255) NOT NULL,
+	LastName	varchar(255) NOT NULL,
+	Mail		varchar(255) NOT NULL,
 	PolId		int FOREIGN KEY REFERENCES Polyclinic(Id),
 	AddedOn		datetime NOT NULL,
 	ModifiedOn	datetime NULL
@@ -45,10 +46,14 @@ VALUES
 
 INSERT INTO Doctor
 VALUES
-(1, 'Utku', 'Kaya', 1, '2016-12-21 00:00:00.000', NULL),
-(2, 'Fatma', 'Aslan', 1,'2016-12-21 00:00:00.000', NULL),
-(3, 'Kemal', 'Baycan', 2,'2016-12-21 00:00:00.000', NULL),
-(4, 'Yaren', 'Atan', 2, '2016-12-21 00:00:00.000', NULL)
+(1, 'Utku', 'Kaya', 'gumus_utku@paint.com', 1, '2016-12-21 00:00:00.000', NULL),
+(2, 'Fatma', 'Aslan', 'yesim_oztas@paint.com', 1, '2016-12-21 00:00:00.000', NULL),
+(3, 'Kemal', 'Baycan', 'baycan_kemal@paint.com', 2, '2016-12-21 00:00:00.000', NULL),
+(4, 'Yaren', 'Atan', 'atan_nazli@paint.com', 2, '2016-12-21 00:00:00.000', NULL),
+(5, 'Fatih',  'Kuyu', 'kuyu_fatih@paint.com', 3, '2016-12-21 00:00:00.000', NULL),
+(6, 'Hatice',  'Sayın', 'sayin_hatice@paint.com', 3, '2016-12-21 00:00:00.000', NULL)
+
+
 
 INSERT INTO Patient	
 VALUES
@@ -62,9 +67,8 @@ CREATE PROC GetAllAppointment
 AS 
 Select count(*) from Appointment;
 
-
-
-/* CREATE PROCEDURE getLastMail
+/*
+CREATE PROCEDURE getLastMail
 AS
 SELECT TOP 1 Patient.Mail FROM Patient INNER JOIN Appointment ON Patient.Id = Appointment.PatientId ORDER BY Appointment.AddedOn DESC
 GO
@@ -73,40 +77,19 @@ CREATE PROCEDURE getLastDate
 AS
 SELECT TOP 1 Appointment.AppointmentDate FROM Appointment ORDER BY Appointment.AddedOn DESC
 GO
+*/
 
-CREATE TRIGGER SendAppMail
-ON dbo.Appointment
-FOR INSERT
-AS 
-BEGIN
-	DECLARE @LastMail varchar(255)
-	EXEC @LastMail = getLastMail
-	DECLARE @LastDate varchar(255)
-	EXEC @LastDate = getLastDate
-	SET NOCOUNT ON
-    BEGIN
-        EXEC msdb.dbo.sp_send_dbmail
-          @recipients = @LastMail, 
-          @profile_name = 'HospitalMail',
-          @subject = 'New Appointment', 
-          @body = 'Your appointment saved. Appointment date is ' -- Düzeltilecek.
-    END
-END
-GO */
-
-Without Procedures
-
-CREATE TRIGGER SendAppMail
+CREATE TRIGGER SendAppointmentGet
 ON Appointment
 AFTER INSERT
 AS 
 BEGIN
 SET NOCOUNT ON;
 	DECLARE @LastMail varchar(255)
-	Select TOP 1 @LastMail=Patient.Mail FROM Patient INNER JOIN Appointment ON Patient.Id = Appointment.PatientId ORDER BY Appointment.AddedOn DESC
+	Select TOP 1 @LastMail = Patient.Mail FROM Patient INNER JOIN Appointment ON Patient.Id = Appointment.PatientId ORDER BY Appointment.AddedOn DESC
 	DECLARE @LastDate varchar(255)
-	SELECT TOP 1 @LastDate=Appointment.AppointmentDate FROM Appointment ORDER BY Appointment.AddedOn DESC
-	DECLARE @body varchar(255)= 'Your appointment saved. Appointment date is ' + @LastDate
+	SELECT TOP 1 @LastDate = Appointment.AppointmentDate FROM Appointment ORDER BY Appointment.AddedOn DESC
+	DECLARE @body varchar(255)= 'Your appointment saved. Appointment date is ' + @LastDate + '. Please be at the hospital 15 minutes before your appointment.'
        EXEC msdb.dbo.sp_send_dbmail
           @recipients = @LastMail, 
           @profile_name = 'HospitalMail',
@@ -114,10 +97,81 @@ SET NOCOUNT ON;
           @body =@body
 END
 
+CREATE TRIGGER SendAppointmentDoctor
+ON Appointment
+AFTER INSERT
+AS 
+BEGIN
+SET NOCOUNT ON;
+	DECLARE @DocMail varchar(255)
+	Select TOP 1 @DocMail = Doctor.Mail FROM Doctor INNER JOIN Appointment ON Doctor.Id = Appointment.DoctorId ORDER BY Appointment.AddedOn DESC
+	DECLARE @LastDate varchar(255)
+	SELECT TOP 1 @LastDate=Appointment.AppointmentDate FROM Appointment ORDER BY Appointment.AddedOn DESC
+	DECLARE @DocName varchar(255)
+	SELECT TOP 1 @DocName = Doctor.FirstName + ' ' + Doctor.LastName FROM Doctor INNER JOIN Appointment ON Doctor.Id = Appointment.DoctorId ORDER BY Appointment.AddedOn DESC
+	DECLARE @body varchar(255)= 'Doctor ' + @DocName + ',' + CHAR(13) + CHAR(10) +'You have a new appointment on ' + @LastDate + '. For your information.'
+       EXEC msdb.dbo.sp_send_dbmail
+          @recipients = @DocMail, 
+          @profile_name = 'HospitalMail',
+          @subject = 'New Appointment', 
+          @body =@body
+END
 
+CREATE TRIGGER SendAppointmentDelete
+ON Appointment
+AFTER DELETE
+AS 
+BEGIN
+SET NOCOUNT ON;
+	DECLARE @LastMail varchar(255)
+	Select TOP 1 @LastMail = Patient.Mail FROM Patient INNER JOIN deleted ON Patient.Id = deleted.PatientId ORDER BY deleted.AddedOn DESC
+	DECLARE @LastDate varchar(255)
+	SELECT TOP 1 @LastDate = deleted.AppointmentDate FROM deleted ORDER BY deleted.AddedOn DESC
+	DECLARE @body varchar(255)= 'Your appointment on ' + @LastDate + ' has been cancelled.'
+       EXEC msdb.dbo.sp_send_dbmail
+          @recipients = @LastMail, 
+          @profile_name = 'HospitalMail',
+          @subject = 'Appointment Cancel', 
+          @body =@body
+END
 
+CREATE TRIGGER SendAppointmentCancelDoctor
+ON Appointment
+AFTER DELETE
+AS 
+BEGIN
+SET NOCOUNT ON;
+	DECLARE @DocMail varchar(255)
+	Select TOP 1 @DocMail = Doctor.Mail FROM Doctor INNER JOIN deleted ON Doctor.Id = deleted.DoctorId ORDER BY deleted.AddedOn DESC
+	DECLARE @LastDate varchar(255)
+	SELECT TOP 1 @LastDate = deleted.AppointmentDate FROM deleted ORDER BY deleted.AddedOn DESC
+	DECLARE @DocName varchar(255)
+	SELECT TOP 1 @DocName = Doctor.FirstName + ' ' + Doctor.LastName FROM Doctor INNER JOIN deleted ON Doctor.Id = deleted.DoctorId ORDER BY deleted.AddedOn DESC
+	DECLARE @body varchar(255)= 'Doctor ' + @DocName + ',' + CHAR(13) + CHAR(10) +'Your appointment on ' + @LastDate + ' has been cancelled. For your information.'
+       EXEC msdb.dbo.sp_send_dbmail
+          @recipients = @DocMail, 
+          @profile_name = 'HospitalMail',
+          @subject = 'Appointment Cancel', 
+          @body =@body
+END
 
-Create Procedure GetAppointmentsDetailByDateRange @PatientId varchar(11), @StartingTime DateTime
+CREATE PROCEDURE IncSalaryDocsMail
 AS
-Select AppointmentDate, Doctor.FirstName+' ' +Doctor.LastName as DoctorName,Polyclinic.Name as PolyclinicName from Appointment,Doctor,Polyclinic 
-where Appointment.PatientId=@PatientId and Appointment.DoctorId=Doctor.Id and Polyclinic.Id=Doctor.PolId and Appointment.AddedOn Between @StartingTime and GETDATE();
+	DECLARE @Mail varchar(255)
+	DECLARE @DocName varchar(255)
+	DECLARE @Count int = 1
+	DECLARE @Last int
+	SELECT TOP 1 @Last = Doctor.Id FROM Doctor ORDER BY Id DESC
+	WHILE @Count <= @Last
+		BEGIN
+			SELECT @Mail = Doctor.Mail, @DocName = Doctor.FirstName + ' ' + Doctor.LastName FROM Doctor WHERE Id = @Count 
+			DECLARE @body varchar(255)= 'Doctor ' + @DocName + ',' + CHAR(13) + CHAR(10) + 'Your salary has been increased by $250. Stay healthy.'
+				EXEC msdb.dbo.sp_send_dbmail
+					@recipients = @Mail, 
+					@profile_name = 'HospitalMail',
+					@subject = 'Salary Update', 
+					@body =@body
+			SET @Count += 1
+		END
+
+
