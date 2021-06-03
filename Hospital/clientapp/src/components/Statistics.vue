3a9d664aa44981c3b55e8c90502278b9eb5da874
Osman Yasir Çankaya <osmancankaya97@hotmail.com>
2021-06-04 00:50:54 +0300

    statistics

--- Hospital/clientapp/src/components/Statistics.vue
+++ Hospital/clientapp/src/components/Statistics.vue
@@ -1,25 +1,221 @@
 <template>
-  <div>
-    <h1 class="mb-5" style="color: #FFFFFF">İSTATİSTİKLER</h1>
-    <!-- <div class="d-flex justify-content-center mt-5 mb-5">
-      <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">Kimlik Numarası</th>
-              <th class="text-left">Kayıt Tarihi</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in patients" :key="item.citizenNumber">
-              <td>{{ item.id }}</td>
-              <td>{{ item.addedOn }}</td>
-            </tr>
-          </tbody>
+  <v-app style="background-image: url(background.png); background-size: cover">
+    <h1 class="mb-5" style="color: #ffffff">İSTATİSTİKLER</h1>
+    <div class="d-flex justify-content-center mt-5 mb-5">
+      <v-dialog v-model="dialog1" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#ffffff" outlined v-bind="attrs" class="mr-5" v-on="on" width="500px">
+            Randevu Sayısına Göre Tarihlerin Sıralaması
+          </v-btn>
         </template>
-      </v-simple-table>
-    </div> -->
-  </div>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Tarih</th>
+                <th class="text-left">Randevu Adedi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(item, index) in appointmensCountOrderByDate"
+                :key="index"
+              >
+                <td>{{ item.Date }}</td>
+                <td>{{ item.AppointmentCount }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-dialog>
+      <v-dialog v-model="dialog2" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#ffffff" outlined v-bind="attrs" v-on="on" width="500px">
+            En Çok Randevusu Olan Doktorlar
+          </v-btn>
+        </template>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Doktor</th>
+                <th class="text-left">Randevu Adedi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(item, index) in doctorsOrderByAppointmentCount"
+                :key="index"
+              >
+                <td>{{ item.FullName }}</td>
+                <td>{{ item.AppointmentCount }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-dialog>
+    </div>
+    <div class="d-flex justify-content-center mt-5 mb-5">
+      <v-dialog v-model="dialog3" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#ffffff" outlined v-bind="attrs" class="mr-5" v-on="on" width="500px">
+            Randevu Sayısına Göre Poliklinikler
+          </v-btn>
+        </template>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Poliklinik Adı</th>
+                <th class="text-left">Randevu Adedi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(item, index) in polyclinicNameByAppointmentCount"
+                :key="index"
+              >
+                <td>{{ item.Name }}</td>
+                <td>{{ item.AppointmentCount }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-dialog>
+      <v-dialog v-model="dialog4" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#ffffff" outlined v-bind="attrs" v-on="on" width="500px">
+            Randevu Sayısına Göre Günler
+          </v-btn>
+        </template>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Gün</th>
+                <th class="text-left">Randevu Adedi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(item, index) in weekDayByAppointmentCount"
+                :key="index"
+              >
+                <td>{{ item.GUN }}</td>
+                <td>{{ item.AppointmentCount }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-dialog>
+    </div>
+    <div class="d-flex justify-content-center mt-5 mb-5">
+      <v-dialog v-model="dialog5" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#ffffff" outlined v-bind="attrs" class="mr-5" v-on="on" width="500px">
+            Saatlere Göre Randevu Sayıları
+          </v-btn>
+        </template>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Saat</th>
+                <th class="text-left">Randevu Adedi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in appointmentCountByHours" :key="index">
+                <td>{{ item.HOURS }}</td>
+                <td>{{ item.AppointmentCount }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-dialog>
+      <v-dialog v-model="dialog6" width="300px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#ffffff" outlined v-bind="attrs" v-on="on" width="500px">
+            Doktorlara Göre Randevu Sayıları
+          </v-btn>
+        </template>
+        <div style="background-color: #ffffff; height: 150px">
+          <v-col cols="12" sm="12">
+            <v-select></v-select>
+            <span>{{ appointmentsSizeByDoctorId }}</span>
+          </v-col>
+        </div>
+      </v-dialog>
+    </div>
+    <div class="d-flex justify-content-center mt-5 mb-5">
+      <v-dialog v-model="dialog7" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#ffffff" outlined v-bind="attrs" class="mr-5" v-on="on" width="500px">
+            Seçilen Hastanın Randevuları
+          </v-btn>
+        </template>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Randevu Tarihi</th>
+                <th class="text-left">Doktor Adı</th>
+                <th class="text-left">Poliklinik Adı</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(item, index) in appointmentsDetailByDateRange"
+                :key="index"
+              >
+                <td>{{ item.AppointmentDate }}</td>
+                <td>{{ item.DoctorName }}</td>
+                <td>{{ item.PolyclinicName }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-dialog>
+      <v-dialog v-model="dialog10" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#ffffff" outlined v-bind="attrs" v-on="on" width="500px">
+            Toplam Randevu Sayısı
+          </v-btn>
+        </template>
+        <div style="background-color: #ffffff; height: 50px">
+          <h5>{{ appointmentsCount }} Randevu</h5>
+        </div>
+      </v-dialog>
+    </div>
+    <div class="d-flex justify-content-center mt-5 mb-5">
+      <v-dialog v-model="dialog9" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#ffffff" outlined v-bind="attrs" class="mr-5" v-on="on" width="500px">
+            En Çok Randevu Alınan Tarih
+          </v-btn>
+        </template>
+        <div style="background-color: #ffffff; height: 50px">
+          <h5>
+            {{ maximumAppointmentDay.Date }}:
+            {{ maximumAppointmentDay.AppointmentCount }} Randevu
+          </h5>
+        </div>
+      </v-dialog>
+      <v-dialog v-model="dialog10" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#ffffff" outlined v-bind="attrs" v-on="on" width="500px">
+            En Az Randevu Alınan Tarih
+          </v-btn>
+        </template>
+        <div style="background-color: #ffffff; height: 50px">
+          <h5>
+            {{ minimumAppointmentDay.Date }}:
+            {{ minimumAppointmentDay.AppointmentCount }} Randevu
+          </h5>
+        </div>
+      </v-dialog>
+    </div>
+  </v-app>
 </template>
 
 <script>
@@ -29,6 +225,16 @@ export default {
   name: "Statistics",
   data() {
     return {
+      dialog1: false,
+      dialog2: false,
+      dialog3: false,
+      dialog4: false,
+      dialog5: false,
+      dialog6: false,
+      dialog7: false,
+      dialog8: false,
+      dialog9: false,
+      dialog10: false,
       appointmensCountOrderByDate: [],
       doctorsOrderByAppointmentCount: [],
       maximumAppointmentDay: {},
@@ -38,20 +244,20 @@ export default {
       appointmentCountByHours: [],
       appointmentsSizeByDoctorId: 0,
       appointmentsDetailByDateRange: [],
-      appointmentsCount: 0
+      appointmentsCount: 0,
     };
   },
   created() {
-    this.getAppointmensCountOrderByDate()
-    this.getDoctorsOrderByAppointmentCount()
-    this.getMaximumAppointmentDay()
-    this.getMinimumAppointmentDay()
-    this.getPolyclinicNameByAppointmentCount()
-    this.getWeekDayByAppointmentCount()
-    this.getAppointmentCountByHours()
-    this.getAppointmentsSizeByDoctorId()
-    this.getAppointmentsDetailByDateRange()
-    this.getAppointmentsCount()
+    this.getAppointmensCountOrderByDate();
+    this.getDoctorsOrderByAppointmentCount();
+    this.getMaximumAppointmentDay();
+    this.getMinimumAppointmentDay();
+    this.getPolyclinicNameByAppointmentCount();
+    this.getWeekDayByAppointmentCount();
+    this.getAppointmentCountByHours();
+    this.getAppointmentsSizeByDoctorId();
+    this.getAppointmentsDetailByDateRange();
+    this.getAppointmentsCount();
   },
   methods: {
     getAppointmensCountOrderByDate() {
@@ -122,7 +328,9 @@ export default {
 
     getAppointmentsSizeByDoctorId() {
       ApiService.setHeader();
-      ApiService.get("api/Statistic/GetAppointmentsSizeByDoctorId/" + 3 + "/" + 30)
+      ApiService.get(
+        "api/Statistic/GetAppointmentsSizeByDoctorId/" + 3 + "/" + 30
+      )
         .then((response) => {
           this.appointmentsSizeByDoctorId = response.data;
         })
@@ -133,7 +341,12 @@ export default {
 
     getAppointmentsDetailByDateRange() {
       ApiService.setHeader();
-      ApiService.get("api/Statistic/GetAppointmentsDetailByDateRange/" + "21443134132" + "/" + 30)
+      ApiService.get(
+        "api/Statistic/GetAppointmentsDetailByDateRange/" +
+          "21443134132" +
+          "/" +
+          30
+      )
         .then((response) => {
           this.appointmentsDetailByDateRange = response.data;
         })
